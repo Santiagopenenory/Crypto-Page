75fa07b5508805db8982e721cf40154ab785d161
Santiago <penenorysantiago@gmail.com>
2022-05-25 01:59:10 -0300

    Finished v.1

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,81 @@
 <script>
-	export let name;
+	import {onMount} from "svelte";
+	let coins = [];
+	let filteredCoins = [];
+	let titles = [
+		'#',
+		'Coin',
+		'Price',
+		'Price Change',
+		'Volume for 24hs'
+	]
+	let ref = null;
+	let loadCoins = async() => {
+		let response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+		coins =  await response.json();
+		filteredCoins = coins;
+		console.log(coins)
+	}
+	loadCoins()
+	let searchCoin = (value)=>{
+		filteredCoins = coins.filter(coin => coin.name.toLowerCase().includes(value.toLowerCase()) ||
+		coin.symbol.toLowerCase().includes(value.toLowerCase()))
+		console.log(coins)
+	}
+
+	onMount(()=>{
+		ref.focus();
+	})
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div class="container">
+		<div class="row">
+			<h1>CryptoFinder</h1>
+			<input type="text" class="form-control bg-dark text-white rounded-0 border-0 my-4" 
+			placeholder="Search your coin" 
+			on:keyup={({target: {value}}) => searchCoin(value)}
+			bind:this={ref}>
+			<table class="table table-dark table-striped">
+				<thead>
+					<tr>
+						{#each titles as title }
+							<th>{title}</th>
+						{/each}
+					</tr>
+				  </thead>
+				  <tbody>
+					  {#each filteredCoins as coin, i}	  
+						<tr>
+							<td style="color:darkgray;">{i+1}</td>
+							<td>
+								<span>
+									<img src={coin.image} alt={coin.name} style="width: 2rem;" class="img-fluid me-2">
+									{coin.name}
+								</span>
+								<span class="text-uppercase ms-2" style="color:dimgrey;">
+									{coin.symbol}
+								</span>
+							</td>
+							<td>
+								${coin.current_price.toLocaleString()}
+							</td>
+							<td class={coin.price_change_percentage_24h>0 ? "text-success" : "text-danger"}>
+								{coin.price_change_percentage_24h} %
+							</td>
+							<td>
+								$ {coin.total_volume.toLocaleString()}
+							</td>
+						</tr>
+					  {/each}
+					
+				  </tbody>
+			</table> 
+		</div>
+	</div>
+
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
