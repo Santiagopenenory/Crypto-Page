75a9473006ad2e4f5bab9719453d043168cbb5b2
Santiago <penenorysantiago@gmail.com>
2022-06-02 01:59:35 -0300

    Change table for cards

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,15 @@
 <script>
 	import {Table} from 'sveltestrap'
 	import { Styles } from 'sveltestrap';
-	import {onMount} from "svelte";
+	import {
+    Card,
+    CardBody,
+    CardSubtitle,
+    CardTitle,
+	Input
+  } from 'sveltestrap';
 	let coins = [];
 	let filteredCoins = [];
-	let titles = [
-		'#',
-		'Coin',
-		'Price',
-		'Price Change',
-		'Volume for 24hs'
-	]
-	let ref = null;
 	let loadCoins = async() => {
 		let response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
 		coins =  await response.json();
@@ -23,9 +21,7 @@
 		coin.symbol.toLowerCase().includes(value.toLowerCase()))
 	}
 
-	onMount(()=>{
-		ref.focus();
-	})
+	
 </script>
 
 <svelte:head>
@@ -33,51 +29,31 @@
 </svelte:head>
 
 <main>
-	<div class="container">
-		<div class="row">
-			<h1>CryptoFinder</h1>
-			<input type="text" class="form-control bg-dark text-white rounded-0 border-0 my-4" 
-			placeholder="Search your coin" 
-			on:keyup={({target: {value}}) => searchCoin(value)}
-			bind:this={ref}>
-			<Table bordered dark striped responsive>
-				<thead>
-					<tr>
-						{#each titles as title }
-							<th>{title}</th>
-						{/each}
-					</tr>
-				</thead>
-				<tbody>
-					{#each filteredCoins as coin, i}	  
-						<tr>
-							<td style="color:darkgray;">{i+1}</td>
-							<td>
-								<span>
-									<img src={coin.image} alt={coin.name} style="width: 2rem;" class="img-fluid me-2">
-									{coin.name}
-								</span>
-								<span class="text-uppercase ms-2" style="color:dimgrey;">
-									{coin.symbol}
-								</span>
-							</td>
-							<td>
-								${coin.current_price.toLocaleString()}
-							</td>
-							<td class={coin.price_change_percentage_24h>0 ? "text-success" : "text-danger"}>
-								{coin.price_change_percentage_24h} %
-							</td>
-							<td>
-								$ {coin.total_volume.toLocaleString()}
-							</td>
-						</tr>
-					{/each}
-					
-				</tbody>
-			</Table>
-		</div>
+	<h1>CryptoFinder</h1>
+	<div class="container-lg">
+		<Input class="form-control bg-dark text-white rounded-0 border-0 my-4 "
+		placeholder="Search your coin" 
+		on:keyup={({target: {value}}) => searchCoin(value)} style="max-width:100%;">
+	</Input>
 	</div>
-
+		<div class="container">
+			{#each filteredCoins as coin, i}	  
+				<Card style="width: 300px;" class="mb-3 bg-dark" >
+					<div class="row g-0 text-center">
+						<div class="col-md-4"><br>
+							<img src= {coin.image} alt={coin.name} style="padding-left:20px;" class="img-fluid rounded-start">
+						</div>
+						<div class="col-md-8">
+							<CardBody>
+								<CardTitle class="bs-light ">{coin.name}</CardTitle><br>
+								<CardSubtitle>${coin.current_price.toLocaleString()}</CardSubtitle><br>
+								<CardSubtitle class={coin.price_change_percentage_24h>0 ? "text-success" : "text-danger"}>{coin.price_change_percentage_24h} %</CardSubtitle>
+							</CardBody>
+						</div>
+					</div>
+				</Card>
+			{/each}
+	</div>
 </main>
 
 <style>
